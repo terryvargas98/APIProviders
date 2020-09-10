e228d17e7378bc9ae8f0589419ea909941967e62
Terry Alberto Vargas Santa Cruz <terry.vargas@unas.edu.pe>
2020-09-10 12:20:39 -0500

    make it betted

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         return $next($request)
        //Url a la que se le dará acceso en las peticiones
-      ->header("Access-Control-Allow-Origin", "https://laravelapirestfull.herokuapp.com/api")
+      ->header("Access-Control-Allow-Origin", "https://laravelapirestfull.herokuapp.com")
       //Métodos que a los que se da acceso
       ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
       //Headers de la petición
